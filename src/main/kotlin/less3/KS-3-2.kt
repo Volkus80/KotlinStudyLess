bc2b1485c1865b06bb28645c7dd517ea102269f3
Volkus <vkazakov@robotx.ru>
2025-04-04 10:12:55 +0200

    KS-3-2 person data changes

---
+++ src/main/kotlin/less3/KS-3-2.kt
@@ -0,0 +1,15 @@
+package ru.volkus.rate.less3
+
+fun main() {
+    val maidenName = "Андреева"
+    val marriedName = "Сидорова"
+
+    val fullName = "Татьяна Сергеевна"
+
+    var age = 20
+    println("$maidenName $fullName, $age")
+
+    age = 22
+    println("$marriedName $fullName, $age")
+
+}
