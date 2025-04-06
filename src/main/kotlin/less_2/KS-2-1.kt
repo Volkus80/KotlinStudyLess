f5d9525ea40c40b4f7d9192c3aede97df2b49719
volkus <volkus80@gmail.com>
2025-04-06 13:35:12 +0200

    KS-2-5 cont deposit sum

--- src/main/kotlin/less_2/KS-2-1.kt
+++ src/main/kotlin/less_2/KS-2-1.kt
@@ -1,6 +1,11 @@
 package ru.volkus.rate.less_2
 
 fun main() {
-    val averageMark = (3 + 4 + 3 + 5) / 4.0
-    println(String.format("%.2f", averageMark))
+    val pupilsQuantity = 4
+    val firstsMark = 3.0
+    val secondsMark = 4.0
+    val thirdsMark = 3.0
+    val fourthMark = 5.0
+    val averageMark = (firstsMark + secondsMark + thirdsMark + fourthMark) / pupilsQuantity
+    println("%.2f".format(averageMark))
 }
